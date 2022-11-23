b178969550c7e3152eb06226d5cfa8ac73bc1210
Mehdi Benouhoud <mehdi.benouhoud@polymtl.ca>
2022-11-23 09:29:42 -0500

    Clean up FSM_RPC.vhd

--- TP05/FSM_RPC.vhd
+++ TP05/FSM_RPC.vhd
@@ -62,70 +62,13 @@ begin
     process(CLK, RST)
     begin
     
-        if(CLK='1' and CLK'event) then
-            if(RST='1') then 
-                --state <= state_0;
-                STATE <= IDLE;
-            elsif (V='1' and STATE=IDLE) then
-                STATE <= PLAYER1;
-            elsif (V='1' and STATE=PLAYER1) then
-                STATE <= PLAYER2;
-            elsif (R='1' and (STATE = PLAYER1 OR STATE = PLAYER2)) then
-                SELECTED_GAME_SIGN <= ROCK;
-                GAME_SIGN_DISPLAY_VECTOR <= "10000";
-            elsif (P='1' and (STATE = PLAYER1 OR STATE = PLAYER2)) then
-                SELECTED_GAME_SIGN <= PAPER;
-                GAME_SIGN_DISPLAY_VECTOR <= "10001";
-            elsif (C='1' and (STATE = PLAYER1 OR STATE = PLAYER2)) then
-                SELECTED_GAME_SIGN <= SCISSORS;
-                GAME_SIGN_DISPLAY_VECTOR <= "10010";
-            
-            
-                --state <= next_state;
-            end if;
-        end if;
+        
     end process;
 -- FIN - Partie séquentielle - Implémentation registre
 
 process(STATE, V)
 begin
-    case STATE is
-        when IDLE =>
-        Disp0 <= "11110";
-        Disp1 <= "11110";
-        Disp2 <= "11110";
-        Disp3 <= "11110";
-        Disp4 <= "11110";
-        Disp5 <= "11110";
-        Disp6 <= "11110";
-        Disp7 <= "11110";
-        
-        when PLAYER1 =>
-        
-        
-        Disp7 <= "10001";
-        Disp6 <= "00001";
-        Disp5 <= "11111";
-        Disp4 <= "11111";
-        
-        Disp3 <= GAME_SIGN_DISPLAY_VECTOR;
-        Disp2 <= GAME_SIGN_DISPLAY_VECTOR;
-        Disp1 <= GAME_SIGN_DISPLAY_VECTOR;
-        Disp0 <= GAME_SIGN_DISPLAY_VECTOR;
-        when PLAYER2 =>
-        
-        Disp7 <= "10001";
-        Disp6 <= "00010";
-        Disp5 <= "11111";
-        Disp4 <= "11111";
-        
-        Disp3 <= GAME_SIGN_DISPLAY_VECTOR;
-        Disp2 <= GAME_SIGN_DISPLAY_VECTOR;
-        Disp1 <= GAME_SIGN_DISPLAY_VECTOR;
-        Disp0 <= GAME_SIGN_DISPLAY_VECTOR;
-        
-        when RESULT =>
-    end case;
+   
         
 end process;
 
